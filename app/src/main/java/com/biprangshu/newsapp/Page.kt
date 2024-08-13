dbf6a1d4d049ba488b1478bb2d0991aa39650ab2
Biprangshu Das <dasbiprangshu@gmail.com>
2024-08-13 21:56:55 +0530

    Added text to the onboarding screens

--- app/src/main/java/com/biprangshu/newsapp/Page.kt
+++ app/src/main/java/com/biprangshu/newsapp/Page.kt
@@ -12,18 +12,18 @@ data class Page(
 
 val pages= listOf(
     Page(
-        title = "Lorem Ipsum",
-        description = "lorem Ipsum",
+        title = "Welcome to News App",
+        description = "The one stop app to make yourself updated to the latest in the World",
         Image = R.drawable.onboarding1
     ),
     Page(
-        title = "Lorem Ipsum",
-        description = "lorem Ipsum",
+        title = "Read, Discover, Save",
+        description = "Read all the latest article from reputed sources in one place, and save them if you like it!",
         Image = R.drawable.onboarding2
     ),
     Page(
-        title = "Lorem Ipsum",
-        description = "lorem Ipsum",
+        title = "Get, Set and Go",
+        description = "And start your news reading journey, A habit you can never forget!",
         Image = R.drawable.onboarding3
     )
     
