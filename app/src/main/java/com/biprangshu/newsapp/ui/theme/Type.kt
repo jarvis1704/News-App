ac894f1657940736e744ca053d96e2169b348373
Biprangshu Das <dasbiprangshu@gmail.com>
2025-04-03 19:34:27 +0530

    Included the fonts

--- app/src/main/java/com/biprangshu/newsapp/ui/theme/Type.kt
+++ app/src/main/java/com/biprangshu/newsapp/ui/theme/Type.kt
@@ -2,9 +2,12 @@ package com.biprangshu.newsapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.biprangshu.newsapp.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +34,28 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val Merriweather= FontFamily(
+    Font(R.font.merriweather_variablefont),
+    Font(R.font.merriweather_italic, style = FontStyle.Italic)
+)
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_thin, weight = FontWeight.Thin),
+    Font(R.font.montserrat_thinitalic, weight = FontWeight.Thin, style = FontStyle.Italic),
+    Font(R.font.montserrat_extralight, weight = FontWeight.ExtraLight),
+    Font(R.font.montserrat_extralightitalic, weight = FontWeight.ExtraLight, style = FontStyle.Italic),
+    Font(R.font.montserrat_light, weight = FontWeight.Light),
+    Font(R.font.montserrat_lightitalic, weight = FontWeight.Light, style = FontStyle.Italic),
+    Font(R.font.montserrat_regular, weight = FontWeight.Normal),
+    Font(R.font.montserrat_italic, weight = FontWeight.Normal, style = FontStyle.Italic),
+    Font(R.font.montserrat_medium, weight = FontWeight.Medium),
+    Font(R.font.montserrat_mediumitalic, weight = FontWeight.Medium, style = FontStyle.Italic),
+    Font(R.font.montserrat_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.montserrat_semibolditalic, weight = FontWeight.SemiBold, style = FontStyle.Italic),
+    Font(R.font.montserrat_bold, weight = FontWeight.Bold),
+    Font(R.font.montserrat_bolditalic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(R.font.montserrat_extrabold, weight = FontWeight.ExtraBold),
+    Font(R.font.montserrat_extrabolditalic, weight = FontWeight.ExtraBold, style = FontStyle.Italic)
 )
